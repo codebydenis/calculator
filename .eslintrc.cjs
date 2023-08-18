d047c897a6aaa56bdeec5df6b6eabbb86ce6de61
Denis <dev.denispatlin@gmail.com>
2023-08-19 02:26:37 +0600

    настроил prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:vue/vue3-strongly-recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
-  rules: {
-    "no-console": "warn",
-    "no-debugger": "warn",
-  },
+    extends: [
+        'plugin:vue/vue3-strongly-recommended',
+        'eslint:recommended',
+        'plugin:prettier/recommended',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        'prettier/prettier': [
+            'error',
+            {
+                tabWidth: 4,
+                endOfLine: 'auto',
+                singleQuote: true,
+            },
+        ],
+        'no-console': 'warn',
+        'no-debugger': 'warn',
+    },
 };
