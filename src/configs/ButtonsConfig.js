539047fadf8b62b4cf533a9fcd1933dbfa15fe7c
Denis <dev.denispatlin@gmail.com>
2023-09-04 07:49:18 +0600

    конфиг кнопок калькулятора

---
+++ src/configs/ButtonsConfig.js
@@ -0,0 +1,106 @@
+export default {
+    actions: [
+        {
+            value: 'clear',
+            text: 'C',
+        },
+        {
+            value: '+/-',
+            icon: true,
+            iconName: 'IconPlusMinus',
+        },
+        {
+            value: '%',
+            text: '%',
+        },
+    ],
+    numbers: [
+        {
+            type: 'number',
+            value: 7,
+            text: 7,
+        },
+        {
+            type: 'number',
+            value: 8,
+            text: 8,
+        },
+        {
+            type: 'number',
+            value: 9,
+            text: 9,
+        },
+        {
+            type: 'number',
+            value: 4,
+            text: 4,
+        },
+        {
+            type: 'number',
+            value: 5,
+            text: 5,
+        },
+        {
+            type: 'number',
+            value: 6,
+            text: 6,
+        },
+        {
+            type: 'number',
+            value: 1,
+            text: 1,
+        },
+        {
+            type: 'number',
+            value: 2,
+            text: 2,
+        },
+        {
+            type: 'number',
+            value: 3,
+            text: 3,
+        },
+    ],
+    special: [
+        {
+            value: '.',
+            text: '.',
+        },
+        {
+            type: 'number',
+            value: 0,
+            text: '0',
+        },
+        {
+            value: 'backspace',
+            icon: true,
+            iconName: 'IconBackspace',
+        },
+    ],
+    operators: [
+        {
+            value: '/',
+            icon: true,
+            iconName: 'IconDivide',
+        },
+        {
+            value: '*',
+            icon: true,
+            iconName: 'IconMultiply',
+        },
+        {
+            value: '-',
+            icon: true,
+            iconName: 'IconMinus',
+        },
+        {
+            value: '+',
+            icon: true,
+            iconName: 'IconPlus',
+        },
+        {
+            value: '=',
+            text: '=',
+        },
+    ],
+};
